96e6db43821badbb16ff0491bd1d0b41902b1f72
Jack Napolitano <jnapp18@gmail.com>
2015-11-16 13:37:29 -0500

    Added sql script to generate DB
    Added sql script to generate DB. This includes the primary and foreign
    key constraints. Has no triggers at this point.

---
+++ GroceryDB.sql
@@ -0,0 +1,178 @@
+--Drop Tables
+--uncomment to re run .sql
+/*
+DROP TABLE Stock;
+DROP TABLE LineItems;
+DROP TABLE Items;
+DROP TABLE Orders;
+DROP TABLE Customers;
+DROP TABLE DistStation;
+DROP TABLE Warehouse;
+*/
+
+
+CREATE TABLE Warehouse (
+
+WH_ID NUMBER(11) NOT NULL,
+
+Warehouse_Name VARCHAR2(255) NULL,
+
+Street_Address VARCHAR2(255) NULL,
+
+City VARCHAR2(255) NULL,
+
+State VARCHAR2(255) NULL,
+
+Zip_Code VARCHAR2(255) NULL,
+
+Tax_Rate VARCHAR2(255) NULL,
+
+YTD_Sales_Sum VARCHAR2(255) NULL,
+
+PRIMARY KEY (WH_ID) 
+
+);
+
+CREATE TABLE DistStation (
+
+WH_ID NUMBER(11) NOT NULL,
+
+DS_ID NUMBER(11) NOT NULL,
+
+DS_Name VARCHAR2(255) NULL,
+
+Street_Address VARCHAR2(255) NULL,
+
+City VARCHAR2(255) NULL,
+
+State VARCHAR2(255) NULL,
+
+Zip_Code VARCHAR2(255) NULL,
+
+Tax_Rate VARCHAR2(255) NULL,
+
+YTD_Sales_Sum VARCHAR2(255) NULL,
+
+PRIMARY KEY (WH_ID, DS_ID),
+
+CONSTRAINT fk_DistStation_Warehouse_1 FOREIGN KEY (WH_ID) REFERENCES Warehouse (WH_ID)
+
+);
+
+CREATE TABLE Customers (
+
+WH_ID NUMBER(11) NOT NULL,
+
+DS_ID NUMBER(11) NOT NULL,
+
+Cust_ID NUMBER(11) NOT NULL,
+
+First_Name VARCHAR2(255) NULL,
+
+Middle_Initial VARCHAR2(255) NULL,
+
+Last_Name VARCHAR2(255) NULL,
+
+Street_Address VARCHAR2(255) NULL,
+
+City VARCHAR2(255) NULL,
+
+State VARCHAR2(255) NULL,
+
+Zip_code VARCHAR2(255) NULL,
+
+Phone_Number NUMBER(11) NULL,
+
+Sign_Up_Date DATE NULL,
+
+Active_Discount VARCHAR2(255) NULL,
+
+Debt VARCHAR2(255) NULL,
+
+YTD_Purchase_Total VARCHAR2(255) NULL,
+
+Num_Payments VARCHAR2(255) NULL,
+
+Num_Deliveries VARCHAR2(255) NULL,
+
+PRIMARY KEY (DS_ID, Cust_ID),
+
+CONSTRAINT fk_Customers_DistStation_1 FOREIGN KEY (WH_ID, DS_ID) REFERENCES DistStation(WH_ID, DS_ID)
+
+);
+
+CREATE TABLE Orders (
+
+DS_ID NUMBER(11) NOT NULL,
+
+Cust_ID NUMBER(11) NOT NULL,
+
+Order_ID NUMBER(11) NOT NULL,
+
+Date_Placed DATE NULL,
+
+Completed_Flag VARCHAR2(255) NULL,
+
+Num_Items VARCHAR2(255) NULL,
+
+PRIMARY KEY (Cust_ID, Order_ID),
+
+CONSTRAINT fk_Orders_Customers_1 FOREIGN KEY (DS_ID, Cust_ID) REFERENCES Customers (DS_ID, Cust_ID)
+
+);
+
+CREATE TABLE Items (
+
+Item_ID NUMBER NOT NULL,
+
+Item_Name VARCHAR2(255) NULL,
+
+Price NUMBER NULL,
+
+PRIMARY KEY (Item_ID) 
+
+);
+
+CREATE TABLE LineItems (
+
+Cust_ID NUMBER(11) NOT NULL,
+
+Order_ID NUMBER(11) NOT NULL,
+
+LI_ID NUMBER(11) NOT NULL,
+
+Item_ID NUMBER(11) NULL,
+
+Quantity VARCHAR2(255) NULL,
+
+Total_Cost NUMBER NULL,
+
+Date_Delivered DATE NULL,
+
+PRIMARY KEY (Order_ID, LI_ID),
+
+CONSTRAINT fk_LineItems_Orders_1 FOREIGN KEY (Cust_ID, Order_ID) REFERENCES Orders (Cust_ID, Order_ID),
+
+CONSTRAINT fk_LineItems_Items_1 FOREIGN KEY (Item_ID) REFERENCES Items (Item_ID)
+
+);
+
+CREATE TABLE Stock (
+
+WH_ID NUMBER NOT NULL,
+
+Item_ID NUMBER NOT NULL,
+
+Quantity_Available VARCHAR2(255) NULL,
+
+Quantity_Sold VARCHAR2(255) NULL,
+
+Num_Orders VARCHAR2(255) NULL,
+
+PRIMARY KEY (WH_ID, Item_ID),
+
+CONSTRAINT fk_Stock_Warehouse_1 FOREIGN KEY (WH_ID) REFERENCES Warehouse (WH_ID),
+
+CONSTRAINT fk_Stock_Items_1 FOREIGN KEY (Item_ID) REFERENCES Items (Item_ID)
+
+);
--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # CS1555
 Term Project
 
+Finished, needs to be verified:
+-------------------------------
+Create DB from .sql file (jack is working on that right now) 11/16/2015, 12:23 PM
+![alt tag](https://raw.githubusercontent.com/Jnapp18/CS1555/master/ConstraintMappings.jpg?token=AGhooU_B03EGSBQPMFZJhVmUjMDeTS0Eks5WU0vGwA%3D%3D)
+![alt tag](https://raw.githubusercontent.com/Jnapp18/CS1555/master/ConstraintMappings.jpg?token=AGhooU_B03EGSBQPMFZJhVmUjMDeTS0Eks5WU0vGwA%3D%3D)
 
 To-Do
 -------
-Create DB from .sql file (jack is working on that right now) 11/16/2015, 12:23 PM
-![alt tag](https://raw.githubusercontent.com/Jnapp18/CS1555/master/ConstraintMappings.jpg?token=AGhooU_B03EGSBQPMFZJhVmUjMDeTS0Eks5WU0vGwA%3D%3D)
-
-
 Generate Data for DB from java file. (Nick/Jack)
 
 Java file to connect to DB, Jess has skeleton of that.
